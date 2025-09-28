1e0a3a0a6145af049577ec1cfe43772dea1e4638
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-09-28 15:50:59 -0400

    Started utility functions for Javascript

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// alert("hello world");
+// Utility functions for DOM selection 
+
+const $ = s => document.querySelector(s);
+const $$ = s => Array.from.document.querySelectorAll(s);
+const output = $('#output');
+const preview = $('#preview');
+const STORAGE_KEY = 'academy-codelab-web';
+
+const escapeHtml = s => String(s).replace(/[&<>"]/g, c => ({
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;'
+})[c]
+);
+
+function log(msg, type='log') {
+    const color = "error" ? 'var(--err)' : type === 'warn' ? 'var(--warn)' : 'var(--brand)';
+
+    const time = (new Date()).toLocaleTimeString();
+
+    const line = document.createElement('div');
+
+    line.innerHTML = `<span style="color:${color}">[${time}]</span> ${escapeHtml(msg)}`;
+
+    out.appendChild(line);
+    out.scrollTop = out.scrollHeight;
+}
+
+function clearOut() {
+    out.innerHTML = '';
+}
+
+$('#clearOut')?.addEventListener('click', clearOut);
